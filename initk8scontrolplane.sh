621ec1be7edbe7364caf255c538048d4c23685e8
Bent Jensen <bmj@solita.dk>
2025-02-27 07:38:41 +0100

    Envsubst

--- initk8scontrolplane.sh
+++ initk8scontrolplane.sh
@@ -1,3 +1,4 @@
 export CONTROLPLANE_IP=$(hostname -I | awk '{print $1}')
 
-envsubst < kubeadm-config.yaml | sudo kubeadm init --config=-
+envsubst < kubeadm-config.yaml > kubeadm-init-config.yaml
+sudo kubeadm init --config=kubeadm-init-config.yaml
