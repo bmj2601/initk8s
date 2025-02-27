fad69a14de55381f29640ac92a1df56cf4c7198b
Bent Jensen <bmj@solita.dk>
2025-02-27 07:50:21 +0100

    Calico

--- initk8scontrolplane.sh
+++ initk8scontrolplane.sh
@@ -2,3 +2,10 @@ export CONTROLPLANE_IP=$(hostname -I | awk '{print $1}')
 
 envsubst < kubeadm-config.yaml > kubeadm-init-config.yaml
 sudo kubeadm init --config=kubeadm-init-config.yaml
+
+mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+
+kubectl apply -f calico.yaml
